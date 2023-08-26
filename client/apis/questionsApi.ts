b78b2244f72fe199b1a9720c5bf80334228606fa
Rigelle Cruz <rigellecruz@gmail.com>
2023-08-26 15:46:06 +1200

    make an api call to retrieve questions

--- client/apis/questionsApi.ts
+++ client/apis/questionsApi.ts
@@ -2,7 +2,10 @@ import request from 'superagent'
 
 const baseUrl = '/api/v1/questions'
 
-export async function fetchedQuestions() {
-  const res = await request.get(baseUrl)
-  return res.body
+const fetchTriviaQuestions = async () => {
+  const response = await request.get(
+    'https://opentdb.com/api.php?amount=50&category=15&type=boolean'
+  )
+
+  return response.body
 }
