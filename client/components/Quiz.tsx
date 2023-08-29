8aca6e3dc5e643b0daa5d5e7a1d53c97d6a4dbe3
Rigelle Cruz <rigellecruz@gmail.com>
2023-08-30 10:10:56 +1200

    set up useStates for game logic

--- client/components/Quiz.tsx
+++ client/components/Quiz.tsx
@@ -1,10 +1,17 @@
+import { useState } from 'react'
+
 import Questions from './Questions'
 
 function Quiz() {
+  const [score, setScore] = useState(0)
+  const [questionIndex, setQuestionIndex] = useState(0)
+  const [gameOver, setGameOver] = useState(false)
+
   return (
     <>
       <h1>QUIZ</h1>
       <Questions />
+      {/* make conditional rendering */}
     </>
   )
 }
