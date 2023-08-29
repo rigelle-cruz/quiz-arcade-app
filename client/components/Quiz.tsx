970bc9d578b48cfba18f12f793916f819ae5fb19
Rigelle Cruz <rigellecruz@gmail.com>
2023-08-30 10:35:42 +1200

    fetch trivia questions from API

--- client/components/Quiz.tsx
+++ client/components/Quiz.tsx
@@ -1,11 +1,27 @@
 import { useState } from 'react'
+import { useQuery } from 'react-query'
 
 import Questions from './Questions'
+import { fetchTriviaQuestions } from '../apis/questionsApi'
 
 function Quiz() {
+  const { data, error, isLoading } = useQuery(
+    'triviaQuestions',
+    fetchTriviaQuestions
+  )
   const [score, setScore] = useState(0)
   const [questionIndex, setQuestionIndex] = useState(0)
   const [gameOver, setGameOver] = useState(false)
+
+  if (isLoading) {
+    return <div>Loading...</div>
+  }
+
+  if (error instanceof Error) {
+    return <div>Error: {error.message}</div>
+  }
+
+  const currentQuestion = data.results[currentQuestionIndex]
 
   return (
     <>
