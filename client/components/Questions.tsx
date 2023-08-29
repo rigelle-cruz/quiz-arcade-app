30a8e7e1c905d39bc5cea842f4cfce38c8c52413
Rigelle Cruz <rigellecruz@gmail.com>
2023-08-30 10:52:22 +1200

    implement ability to handle answers

--- client/components/Questions.tsx
+++ client/components/Questions.tsx
@@ -8,7 +8,17 @@ interface QuestionProps {
   onAnswer: (isCorrect: boolean) => void
 }
 
-function Questions() {
+function Questions({ question, onAnswer }: QuestionProps) {
+  const [answered, setAnswered] = useState(false)
+  const handleAnswer = (isTrue: boolean) => {
+    if (isTrue === (question.correct_answer === 'True')) {
+      onAnswer(true) // Correct answer, move to the next question
+    } else {
+      onAnswer(false) // Incorrect answer, trigger Game Over
+    }
+    setAnswered(true) // Mark the question as answered
+  }
+
   return <div></div>
 }
 
