f8804cf5f4436a22312989ed994573dd56885488
Rigelle Cruz <rigellecruz@gmail.com>
2023-08-30 10:50:25 +1200

    remove fetchTriviaQuestions

--- client/components/Questions.tsx
+++ client/components/Questions.tsx
@@ -1,5 +1,4 @@
 import { useQuery } from 'react-query'
-import { fetchTriviaQuestions } from '../apis/questionsApi'
 
 interface QuestionProps {
   question: {
@@ -10,29 +9,7 @@ interface QuestionProps {
 }
 
 function Questions() {
-  const { data, error, isLoading } = useQuery(
-    'triviaQuestions',
-    fetchTriviaQuestions
-  )
-
-  if (isLoading) {
-    return <div>Loading...</div>
-  }
-
-  if (error instanceof Error) {
-    return <div>Error: {error.message}</div>
-  }
-
-  return (
-    <div>
-      <h1>Trivia Questions</h1>
-      <ul>
-        {data.results.map((question: any, index: number) => (
-          <li key={index}>{question.question}</li>
-        ))}
-      </ul>
-    </div>
-  )
+  return <div></div>
 }
 
 export default Questions
