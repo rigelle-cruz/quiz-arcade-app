6b6891c78d7913063cdf1d3f68615aa456da8b41
Rigelle Cruz <rigellecruz@gmail.com>
2023-08-31 09:41:40 +1200

    create interface for TriviaQuestion

--- client/apis/questionsApi.ts
+++ client/apis/questionsApi.ts
@@ -2,6 +2,15 @@ import request from 'superagent'
 
 const baseUrl = '/api/v1/questions'
 
+interface TriviaQuestion {
+  category: string
+  type: string
+  difficulty: string
+  question: string
+  correct_answer: string
+  incorrect_answers: string[]
+}
+
 export async function fetchTriviaQuestions() {
   const response = await request.get(
     'https://opentdb.com/api.php?amount=50&category=15&type=boolean'
